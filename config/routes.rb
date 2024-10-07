182c716968907ad842c7ce1bd40aeca5981015f1
carlos concha <carlosconcha@MacBook-Pro-de-carlos.local>
2024-10-07 10:51:09 -0300

    se agrega boostrarp al proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,10 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :new, :create]
   resources :series, only: [:index, :new, :create]
   resources :documentary_films, only: [:index, :new, :create]
-  get "documentary_films/index"
-  get "documentary_films/new"
-  get "documentary_films/create"
-  get "series/index"
-  get "series/new"
-  get "series/create"
-  get "movies/index"
-  get "movies/new"
-  get "movies/create"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
@@ -22,5 +13,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-   root "movie#index"
+  root "movies#index"
 end
